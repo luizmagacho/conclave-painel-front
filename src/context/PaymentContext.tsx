db6ea05204f672df1eeeea348e145be06d5288bd
magacholuizdev <magacholuiz@gmail.com>
2024-03-27 19:47:48 -0300

    Add masks which works

---
+++ src/context/PaymentContext.tsx
@@ -0,0 +1,145 @@
+import {
+  deletePayment,
+  getPayments,
+  getPaymentsByAccountId,
+  postPayment,
+  updatePayment,
+} from "@/services/payment";
+import { Payment, PaymentDTO } from "@/services/payment/type";
+import { ReactNode, createContext, useState } from "react";
+
+interface ProviderProps {
+  children: ReactNode;
+}
+
+interface PaymentContextProps {
+  paymentsByAccountId: Payment[];
+  loading: boolean;
+  totalElements: number;
+  handleGetPayments: (
+    page?: number,
+    beneficiary?: string,
+    paymentDate?: Date
+  ) => Promise<void>;
+  handleGetPaymentsByAccountId: (
+    accountId: string,
+    page?: number,
+    beneficiary?: string,
+    paymentDate?: Date
+  ) => Promise<void>;
+  handlePostPayment: (payment: PaymentDTO) => Promise<void>;
+  handleUpdatePayment: (payment: Payment) => Promise<void>;
+  handleDeletePayment: (paymentId: string) => Promise<void>;
+}
+
+export const PaymentContext = createContext({} as PaymentContextProps);
+
+export const PaymentProvider = ({ children }: ProviderProps) => {
+  const [paymentsByAccountId, setPaymentByAccountId] = useState<Payment[]>([]);
+  const [bufferedPaymentsByAccountId, setBufferedPaymentByAccountId] = useState<
+    Payment[]
+  >([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [totalElements, setTotalElements] = useState<number>(0);
+
+  async function handleGetPayments(
+    page: number = 0,
+    beneficiary: string = "",
+    paymentDate: Date = new Date()
+  ) {
+    setLoading(true);
+    try {
+      const { content, totalElements } = await getPayments({
+        page,
+        size: 5,
+        beneficiary,
+        paymentDate,
+      });
+      setBufferedPaymentByAccountId(content || []);
+      setPaymentByAccountId(content || []);
+      setTotalElements(totalElements);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleGetPaymentsByAccountId(
+    accountId: string,
+    page: number = 0,
+    beneficiary: string = "",
+    paymentDate: Date = new Date()
+  ) {
+    setLoading(true);
+    try {
+      const { content, totalElements } = await getPaymentsByAccountId(
+        {
+          page,
+          size: 5,
+          beneficiary,
+          paymentDate,
+        },
+        accountId
+      );
+      setBufferedPaymentByAccountId(content || []);
+      setPaymentByAccountId(content || []);
+      setTotalElements(totalElements);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handlePostPayment(payment: PaymentDTO) {
+    setLoading(true);
+    try {
+      const resp = await postPayment(payment);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function handleUpdatePayment(payment: Payment) {
+    setLoading(true);
+    try {
+      const resp = await updatePayment(payment);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  async function handleDeletePayment(paymentId: string) {
+    setLoading(true);
+
+    try {
+      const resp = await deletePayment(paymentId);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <PaymentContext.Provider
+      value={{
+        paymentsByAccountId,
+        loading,
+        totalElements,
+        handleGetPayments,
+        handleGetPaymentsByAccountId,
+        handlePostPayment,
+        handleUpdatePayment,
+        handleDeletePayment,
+      }}
+    >
+      {children}
+    </PaymentContext.Provider>
+  );
+};
---
+++ src/services/payment/index.ts
@@ -0,0 +1,56 @@
+import { Pagination } from "@/types/pagination";
+import { getAPIClient } from "../axios";
+import { Payment, PaymentDTO, PaymentPaginationParam } from "./type";
+
+const baseUrl = "/payment";
+const api = getAPIClient();
+
+export async function getPayments({
+  page,
+  size,
+  beneficiary,
+  paymentDate,
+}: PaymentPaginationParam) {
+  let res = await api.get<Pagination<Payment>>(baseUrl, {
+    params: {
+      page,
+      size,
+      beneficiary,
+      paymentDate,
+    },
+  });
+
+  return res.data;
+}
+
+export async function getPaymentsByAccountId(
+  { page, size, beneficiary, paymentDate }: PaymentPaginationParam,
+  accountId: string
+) {
+  let res = await api.get<Pagination<Payment>>(`${baseUrl}/${accountId}`, {
+    params: {
+      page,
+      size,
+      beneficiary,
+      paymentDate,
+    },
+  });
+
+  return res.data;
+}
+
+export async function postPayment(payment: PaymentDTO) {
+  let res = await api.post(baseUrl, payment);
+  return res.status;
+}
+
+export async function updatePayment(payment: Payment) {
+  let res = await api.put(baseUrl, payment);
+  return res.status;
+}
+
+export async function deletePayment(paymentId: string) {
+  let resp = await api.delete(`${baseUrl}/${paymentId}`);
+
+  return resp.status;
+}
