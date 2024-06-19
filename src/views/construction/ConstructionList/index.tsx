0840cd78948466296f65f2235a659d4a95a20013
Luiz Fernando Magacho <luizmagacho94@gmail.com>
2024-06-19 20:38:34 -0300

    Put DataTable as SMALL

--- src/views/construction/ConstructionList/index.tsx
+++ src/views/construction/ConstructionList/index.tsx
@@ -277,7 +277,7 @@ function ConstructionList() {
               stripedRows
               showGridlines
               scrollable
-              scrollHeight="80%"
+              scrollHeight="85vh"
               rows={20}
               totalRecords={totalElements}
               tableStyle={{ minWidth: "50rem" }}
@@ -311,7 +311,7 @@ function ConstructionList() {
               stripedRows
               showGridlines
               scrollable
-              scrollHeight="80%"
+              scrollHeight="85vh"
               rows={10}
               totalRecords={totalElementsNotEnabled}
               tableStyle={{ minWidth: "50rem" }}
--- src/views/user/UserList/index.tsx
+++ src/views/user/UserList/index.tsx
@@ -171,7 +171,7 @@ function UserList() {
           stripedRows
           showGridlines
           scrollable
-          scrollHeight="80%"
+          scrollHeight="85vh"
           rows={10}
           totalRecords={totalElements}
           tableStyle={{ minWidth: "50rem" }}
