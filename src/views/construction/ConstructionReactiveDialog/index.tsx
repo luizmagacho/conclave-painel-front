73e8b0c794efffa94a71ef76cd0c50dcc29bff28
Luiz Fernando Magacho <luizmagacho94@gmail.com>
2024-07-16 19:20:41 -0300

    Add reactive to center cost - Construction

---
+++ src/views/construction/ConstructionReactiveDialog/index.tsx
@@ -0,0 +1,44 @@
+import { Construction } from "@/services/construction/type";
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+
+interface ConstructionReactiveDialog {
+  visible: boolean;
+  onHide: () => void;
+  onReactive: (constructionId: string) => void;
+  data: Construction;
+}
+
+function ConstructionReactiveDialog({
+  visible,
+  onHide,
+  onReactive,
+  data,
+}: ConstructionReactiveDialog) {
+  return (
+    <Dialog
+      header="Reativar Centro de Custo"
+      visible={visible}
+      onHide={onHide}
+      className="w-25rem"
+    >
+      <div className="card flex justify-content-center">
+        <h4>Tem certeza que deseja reativar o centro de custo {data.code} ?</h4>
+      </div>
+      <div className="flex gap-2">
+        <Button className="w-full" label="Cancelar" outlined onClick={onHide} />
+        <Button
+          className="w-full"
+          label="Encerrar"
+          severity="danger"
+          onClick={() => {
+            onReactive(data.id);
+            onHide();
+          }}
+        />
+      </div>
+    </Dialog>
+  );
+}
+
+export default ConstructionReactiveDialog;
